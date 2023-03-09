b7063f389c08e1f985d3514b5dd8d0619272b106
gaurav2k1 <gauravbhukte1507@gmail.com>
2023-03-10 00:01:07 +0530

    fix

--- src/auth/Log.js
+++ src/auth/Log.js
@@ -20,7 +20,9 @@ const Log = () => {
         <div className='w-screen h-screen bg-green-900 flex items-center justify-center'>
             <form className='' onSubmit={setEmailaddress}>
                 <div className='flex items-center gap-4'>
-                    <input className='text-lg px-4 py-2 rounded-lg ' name='email' value={email.email} placeholder="Enter email" onChange={handleChange} />
+                    <input className='text-lg px-4 py-2 rounded-lg ' name='email' value={email.email} placeholder="Enter email" onChange={handleChange} 
+                        autoFocus
+                    />
                     <button className='bg-red-400 text-lg px-4 py-2 rounded-lg' type='submit'>Submit</button>
                 </div>
             </form>
